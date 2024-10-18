dc10508015ae2df97aeaec0efaeebc78a537faa3
FausAstu <feastudi@espol.edu.ec>
2024-10-17 20:23:49 -0700

    Firt commit

---
+++ js/firebase.js
@@ -0,0 +1,24 @@
+/*
+  CCTB Website Development
+  IST105
+  Oct 2024
+  Description: This is a simple login website where students are asked to 
+  implement Social Network Login with Firebase
+*/
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDvkEQkUAIFPwgmDwbUwQZO5BTDEnJaSSU",
+  authDomain: "ist105midtermexam-fausto.firebaseapp.com",
+  projectId: "ist105midtermexam-fausto",
+  storageBucket: "ist105midtermexam-fausto.appspot.com",
+  messagingSenderId: "129746146106",
+  appId: "1:129746146106:web:ca234377e80b4d3fcb3603"
+};
+
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+
